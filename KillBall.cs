702f6cb7271da083205c79f5f5dc2bd7cf9f9885
andy <andybone96@hotmail.com>
2017-09-10 17:10:49 +0100

    Three Target Files

---
+++ KillBall.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillBall : MonoBehaviour {
+
+	public AudioSource deathSound;
+	private BallController theBallController;
+
+	void Start(){
+		theBallController = GameObject.Find ("Ball").GetComponent<BallController> ();
+	}
+
+	void OnTriggerEnter(Collider other){
+		if (other.tag == "Ball") {
+			Instantiate (theBallController.deathExplosion, theBallController.transform.position, theBallController.transform.rotation);
+			deathSound.Play ();
+			ScoreManager.instance.setHighScore ();
+			Destroy (other.gameObject);
+			GameManager.instance.GameOver ();
+		}
+	}
+}
---
+++ SoundTriggerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundTriggerController : MonoBehaviour {
+
+	public AudioSource sonarMedium;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+
+	void OnTriggerEnter(Collider other){
+		if (other.tag == "Ball") {
+			sonarMedium.Play ();
+		}
+	}
+}
